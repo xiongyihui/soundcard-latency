ce6655ef2b7507f4a42c001f1066ce075c51e0dc
Yihui Xiong <yihui.xiong@hotmail.com>
2022-01-21 21:32:28 +0800

    support different sample rates

--- latency.py
+++ latency.py
@@ -8,24 +8,39 @@ import threading
 
 import sounddevice as sd
 import numpy as np
+import resampy
 
 
-rate = 16000
-frame_size = rate // 100
+if len(sys.argv) == 3:
+    sd.default.device = (int(sys.argv[1]), int(sys.argv[2]))
+elif len(sys.argv) != 1:
+    print(f"Usage: python {sys.argv[0]} input_device output_device")
+    sys.exit(1)
 
-sd.default.samplerate = rate
-sd.default.channels = (1, 1)
+input_device_info = sd.query_devices(sd.default.device[0])
+output_device_info = sd.query_devices(sd.default.device[1])
+
+input_rate = int(input_device_info["default_samplerate"])
+output_rate = int(output_device_info["default_samplerate"])
+
+input_channels = input_device_info["max_input_channels"]
+output_channels = output_device_info["max_output_channels"]
+
+print(f'rate {(input_rate, output_rate)}')
+print(f'channels {(input_channels, output_channels)}')
+
+# sd.default.channels = (1, 1)
 sd.default.dtype = ("float32", "float32")
-sd.default.blocksize = frame_size
 sd.default.latency = ("low", "low")
 sd.default.prime_output_buffers_using_stream_callback = True
 
 np.random.seed(random.SystemRandom().randint(1, 1024))
-noise = np.random.normal(0, 1, rate // 10) * np.hanning(rate // 10)
-print(f"noise dtype {noise.dtype}")
-# noise = (noise.astype('float64') * (2**24)).astype('int32')
-zeros = np.zeros(rate // 10)
-source = np.concatenate((zeros, noise, zeros)).astype("float32")
+noise = np.random.normal(0.0, 1.0, output_rate // 10) * np.hanning(output_rate // 10)
+noise = noise.astype("float32")
+zeros = np.zeros(output_rate // 10, dtype="float32")
+mono = np.concatenate((zeros, noise, zeros))
+source = np.zeros((len(mono), output_channels), dtype="float32")
+source[:, 0] = mono
 
 output_index = 0
 output_time = 0
@@ -40,7 +55,7 @@ def callback(data, frames, t, status):
     global adc_time
 
     if not input_blocks:
-        input_time = time.time() - frames / rate
+        input_time = time.time() - frames / input_rate
         adc_time = t.inputBufferAdcTime
         dt = t.currentTime - t.inputBufferAdcTime
         print(f"input latency {dt * 1000} ms")
@@ -66,21 +81,29 @@ def out_callback(outdata, frames, t, status):
     if status:
         print(status)
 
-    data = source[output_index : output_index + frames]
-    size = len(data)
+    data = source[output_index : output_index + frames, :]
+    size = data.shape[0]
     output_index += size
     if size < frames:
-        outdata[:size, 0] = data
-        outdata[size:, 0].fill(0)
+        outdata[:size, :] = data
+        outdata[size:, :].fill(0)
         raise sd.CallbackStop
     else:
-        outdata[:, 0] = data
+        outdata[:, :] = data
 
 
 event = threading.Event()
 
-with sd.InputStream(callback=callback):
+with sd.InputStream(
+    samplerate=input_rate,
+    channels=input_channels,
+    blocksize=input_rate // 100,
+    callback=callback,
+):
     with sd.OutputStream(
+        samplerate=output_rate,
+        channels=output_channels,
+        blocksize=output_rate // 100,
         callback=out_callback,
         finished_callback=event.set,
     ):
@@ -90,7 +113,6 @@ with sd.InputStream(callback=callback):
 
 
 recording = np.concatenate(input_blocks)
-print(recording.dtype)
 print(recording.shape)
 
 
@@ -125,18 +147,21 @@ def gcc_phat(sig, refsig, fs=1, max_tau=None, interp=1):
 
 
 sig = recording[:, 0]
-ref = source
+if input_rate == output_rate:
+    ref = mono
+else:
+    ref = resampy.resample(mono, output_rate, input_rate)
 offset, cc = gcc_phat(sig, ref, fs=1)
 
 
-dt = offset * 1000 / rate
+dt = offset * 1000 / input_rate
 delay = (output_time - input_time) * 1000
 delay2 = (dac_time - adc_time) * 1000
 print(f"dt = {dt} ms")
 print(f"delay = {delay} ms")
 print(f"delay 2 = {delay2} ms")
-print(f"system latency = {dt - delay} ms")
-print(f"system latency 2 = {dt - delay2} ms")
+print(f"latency = {dt - delay} ms")
+print(f"latency 2 = {dt - delay2} ms")
 
 offset = int(offset)
 centre = (len(sig) + len(ref)) // 2 + offset
